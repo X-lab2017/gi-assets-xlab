73552cf9058b9dc2410c3af15e217c98a6a1219e
pomelo.lcw <pomelo.lcw@antgroup.com>
2023-05-16 14:58:20 +0800

    feat: 增加更新机制

---
+++ src/components/XlabLayout/info.ts
@@ -0,0 +1,10 @@
+const info = {
+  id: 'XlabLayout',
+  name: 'Xlab 布局',
+  desc: 'Xlab 布局',
+  cover: 'http://xxx.jpg',
+  category: 'system-interaction',
+  icon: 'icon-home',
+  type: 'GICC_LAYOUT',
+};
+export default info;
